aad61864773d2fa5dbe704bc1d236031ec7dded0
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-04-13 12:10:18 -0500

    prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Hello extends Component {
+  render(){
+    return(<h2>{this.props.title}</h2>)
+  }
+}
+
+class Text extends Component{
+  render(){
+
+    const {
+      arrayOfNumbers,
+      multiply,
+      objetWhitInfo,
+      title
+    } = this.props
+
+    const mappedNumbers = arrayOfNumbers.map(this.props.multiply)
+
+    return(
+    <div>
+      {title}
+      {multiply}
+      <p>{mappedNumbers.join(', ')}</p>
+      <p>{objetWhitInfo.key}</p>
+  
     </div>
-  );
+    )
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <Hello title='Hello from props'/>
+            <Text 
+              arrayOfNumbers = {[2, 3, 10]}
+              objetWhitInfo={{key: 'First Value', key2: 'otherValue'}}
+              multiply={(number) => number*4} 
+              number={2} 
+              text='Texto en string'
+              title={<h1>Este es el titulo</h1>}/>
+        </header>
+      </div>
+    );
+  } 
 }
 
 export default App;
